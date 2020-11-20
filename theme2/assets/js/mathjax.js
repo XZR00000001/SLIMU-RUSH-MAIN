c460a6095250f0b03d476684738176d1bf0f4368
XZR00000001 <37720871+XZR00000001@users.noreply.github.com>
2020-11-20 18:12:48 +0800

    Revert "theme2_update"
    This reverts commit ff16d6b8cb378ca51acc095275f7380f58f8d9f5.

--- theme2/assets/js/mathjax.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    MathJax.Hub.Config({
-        'HTML-CSS': {matchFontHeight: false},
-        SVG: {matchFontHeight: false},
-        CommonHTML: {matchFontHeight: false}
-    });
-});
--- theme2/library/Module/Donate.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (!defined('__TYPECHO_ROOT_DIR__')) exit;
-class Icarus_Module_Donate
-{
-    public static function output()
-    {
-    }
-}
